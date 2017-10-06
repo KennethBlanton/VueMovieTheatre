d9676f69cba1b4ed8f3e92d7a7a8f7064a3f8914
Kenneth <kenneth@creative-mischief.com>
2017-10-06 01:27:25 -0400

    first commit

---
+++ src/components/Overview.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="overview">
+		<DaySelect v-bind:selected="day"></DaySelect>
+        <div class="main">
+            <movie-list v-bind:genre="genre" v-bind:time = "time" v-bind:movies = "movies" v-bind:day = "day"></movie-list>
+            <movie-filter></movie-filter>
+        </div>
+    </div>
+</template>
+<script>
+	import MovieList from './MovieList.vue';
+	import MovieFilter from './MovieFilter.vue';
+	import DaySelect from './DaySelect.vue';
+	export default {
+		props: ['genre', 'time', 'movies', 'day'],
+		components: {
+			MovieList,
+			MovieFilter,
+			DaySelect
+		},
+	}
+</script>
